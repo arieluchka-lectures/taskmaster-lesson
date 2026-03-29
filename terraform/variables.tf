c415a4a0bda53dd2e71ac715d55b38fe522f4092
Ariel Agranovich <ariel.agra@gmail.com>
2026-03-29 15:19:47 +0300

    changed subnet CIDR

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,13 +13,13 @@ variable "project_name" {
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.1.0.0/16"
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
   description = "CIDR block for public subnet"
   type        = string
-  default     = "10.1.1.0/24"
+  default     = "10.0.1.0/24"
 }
 
 variable "instance_type" {
